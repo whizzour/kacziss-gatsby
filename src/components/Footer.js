36a60e787bca686c6bae58a61b845abed52d1851
Lukas Sykora <lukas.sykora@plus4u.net>
2020-04-20 19:21:23 +0200

    changes in nav and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,11 +12,11 @@ const Footer = class extends React.Component {
     return (
       <footer className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
+          {/*<img*/}
+          {/*  src={logo}*/}
+          {/*  alt="Kaldi"*/}
+          {/*  style={{ width: '14em', height: '10em' }}*/}
+          {/*/>*/}
         </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
@@ -37,11 +37,6 @@ const Footer = class extends React.Component {
                     <li>
                       <Link className="navbar-item" to="/products">
                         Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
                       </Link>
                     </li>
                     <li>
@@ -93,13 +88,6 @@ const Footer = class extends React.Component {
                   <img
                     src={instagram}
                     alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
